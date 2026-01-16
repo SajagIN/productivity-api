33948ec8f487ec27ccb3f459061e072608b8d1fa
SajagIN <sajag1403@gmail.com>
2026-01-16 12:52:46 +0530

    User Model

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,20 @@
-// src/server.ts
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
 
-// Load env vars
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors({ origin: "http://localhost:3000" })); 
 app.use(express.json());
 
-// Test route
 app.get("/", (req, res) => {
   res.json({ message: "🚀 Productivity API Running with Bun!" });
 });
 
-// Start server
 const startServer = async () => {
   await connectDB();
   app.listen(PORT, () => {
